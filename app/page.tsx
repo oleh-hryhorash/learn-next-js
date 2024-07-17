ebe569b18664d30a0d8a3dad644f205caaaac509
Oleh <oleghryhorash777@gmail.com>
2024-07-17 16:08:36 +0300

    Full deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,28 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+import Link from 'next/link'; 
+import styles from '@/app/ui/home.module.css'; // Окремі стилі для усіх компонентів
+import clsx from 'clsx'; // Імпортував бібліотеку, що дозволяє дублювати css властивості (наприклад у кнопці: Якщо купив то зелений, якщо ні то сірий колір)
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
+
+/*
+Пропси (props) у Next.js — це об'єкти, які використовуються для передачі даних від батьківського компонента до дочірнього. Вони дозволяють налаштовувати та динамічно змінювати відображення компонентів, роблячи їх більш гнучкими та багаторазовими.
+*/
 
 export default function Page() {
   return (
+   
     <main className="flex min-h-screen flex-col p-6">
+      
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+        <AcmeLogo />
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>  {/* lusitana - це шрифт з fonts.ts який я підключив з google fonts */}
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -23,8 +35,27 @@ export default function Page() {
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
+          <div
+  className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+/>
+<div className={styles.shape} />  {/* styles.shape - це стиль з home.module.css */}
         </div>
+        
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <Image
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+      <Image
+        src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className="block md:hidden"
+        alt="Screenshot of the dashboard project showing mobile version"
+      />
           {/* Add Hero Images Here */}
         </div>
       </div>
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,19 @@
+'use client'; // Вказую, що це клієнтський компонент
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link'; // Імпортую Link, щоб здійснити навігацію на стороні клієнта
+import { usePathname } from 'next/navigation'; // Імпортую хук usePathname
+import clsx from 'clsx';
+
+
+// !!! Link Також дозволяє робити навігацію між сторінками без повного оновлення усіх сторінок, а тільки потрібних компонентів
+/*
+!!!  usePathname() - хук, який можна використовувати для перевірки шляху та реалізації цього шаблону.
+*/
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -17,19 +28,25 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname(); // Присвоїв змінну хуку
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link 
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href, // Використав clsx щоб підсвічувало синім текстом посилання
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+            </Link>
         );
       })}
     </>
